4b4466ee440e67ad932172cbe0005543d697b0d0
ceesprojectofficial <cees.project.official@gmail.com>
2014-06-29 02:01:13 +0200

    Login and store retrieval functionalities added.

--- js/configuration.js
+++ js/configuration.js
@@ -1,20 +1,19 @@
 var config = {
-	//License Key
-	"license_key" : "0000000-000000-0000-0000000000",
+  //License Key
+  "license_key" : "0000000-000000-0000-0000000000",
 
-	//Server definitions
+  //Server definitions
+  "server_url" : "https://80.240.139.49/",
+  "login_endpoint" : "shopassistants/login/",
+  "checkin_endpoint" : "shopassistants/checkin/",
+  "arrivals_endpoint" : "clients/arrivals/",
+  "gcm_endpoint" : "gcm/registration/",
 
-	"server_url" : "https://80.240.139.49/",
-	"login_endpoint" : "shopassistants/login/",
-	"checkin_endpoint" : "shopassistants/checkin/",
-	"arrivals_endpoint" : "clients/arrivals/",
-	"gcm_endpoint" : "gcm/registration/",
+  //Device
+  "mac_address" : "00:0C:29:18:6C:1A",
 
-	//Device
-    "mac_address" : "00:0C:29:18:6C:1A",
-
-    //Registration flag
-    "is_registered" : false
+  //Registration flag
+  "is_registered" : false
 }
 
 function getConfigValues(key){
